763de5ed126170ed8209658f6c5a2a896a192461
Piotr Szmielew <p.szmielew@ava.waw.pl>
2018-06-19 19:04:04 +0200

    missing cmake

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,7 @@ echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
 
 echo "RUN apt-get update"
 echo "RUN apt-get install -y build-essential"
+echo "RUN apt-get install -y cmake"
 
 if [ ! -e $RUBY_VERSION_NUM ] ; then
     echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
